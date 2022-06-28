dd011ff8a17a150256449dcd525a11bd9d03d821
Lucas Vitorino <lucas.vitorino@alterans.tech>
2022-06-28 15:05:30 -0300

    feat(*): event platform

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { Lesson } from "./Lesson";
+import {useGetLessonsQuery} from "../graphql/generated";
+
+export function Sidebar() {
+  const { data } = useGetLessonsQuery();
+
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de Aulas
+      </span>
+
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map((lesson) => {
+          return (
+            <Lesson
+              key={lesson.id}
+              availableAt={new Date(lesson.availableAt)}
+              slug={lesson.slug}
+              title={lesson.title}
+              type={lesson.lessonType}
+            />
+          );
+        })}
+      </div>
+    </aside>
+  );
+}
